729144051733dd36cfd2ddd634dd05752867ac7e
Eric Sowell <eric.sowell@gmail.com>
2017-02-03 14:54:53 -0600

    budget stuff

---
+++ ChartStuff/Controllers/BudgetController.cs
@@ -0,0 +1,61 @@
+﻿using ChartStuff.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ChartStuff.Controllers
+{
+    public class BudgetController : Controller
+    {
+        // GET: Budget
+        public ActionResult Index()
+        {
+            var data = new Budget
+            {
+                Incomes = new List<Income>
+                {
+                    new Income { Source = "Comic Book Sales", Amount = 1000.54M },
+                    new Income { Source = "McDonalds", Amount = 349.43M },
+                },
+                Expenses = new List<Expense>
+                {
+                    new Expense { Name = "Food", Amount = 278.43M },
+                    new Expense { Name = "Fishtank supplies", Amount = 20.60M },
+                    new Expense { Name = "Rent", Amount = 800.00M },
+                    new Expense { Name = "Haircut", Amount = 12.95M }
+                }
+            };
+
+            IEnumerable<Budget> list = new List<Budget> { data };
+
+            //pretend that came from a database
+
+            var budget = list.First();
+
+            var vm = new BudgetIndexViewModel();
+            vm.Budget = budget;
+
+            foreach (var expense in vm.Budget.Expenses)
+            {
+                vm.TotalExpenses += expense.Amount;
+            }
+
+            foreach (var income in vm.Budget.Incomes)
+            {
+                vm.TotalIncome += income.Amount;
+            }
+
+
+            return View(vm);
+        }
+    }
+
+    public class BudgetIndexViewModel
+    {
+        public Budget Budget { get; set; }
+        public decimal TotalExpenses { get; set; }
+        public decimal TotalIncome { get; set; }
+    }
+}
---
+++ ChartStuff/Models/BudgetStuff.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ChartStuff.Models
+{
+    public class Budget
+    {
+        public IEnumerable<Income> Incomes { get; set; }
+        public IEnumerable<Expense> Expenses { get; set; }
+    }
+
+    public class Income
+    {
+        public decimal Amount { get; set; }
+        public string Source { get; set; }
+    }
+
+    public class Expense
+    {
+        public decimal Amount { get; set; }
+        public string Name { get; set; }
+    }
+}
